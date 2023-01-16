55eae1e5f2bf366f0a4f814dc4447a8d107037a8
Mohammad Aless <mhmd.ess@gmail.com>
2023-01-17 00:07:31 +0100

    Add intents

--- index.js
+++ index.js
@@ -5,19 +5,5 @@ var readline = require("readline");
   const dock = await dockStart();
   const nlp = dock.get('nlp');
   await nlp.train();
-
-  var rl = readline.createInterface(process.stdin, process.stdout);
-
-  console.log("Chatbot started!");
-  rl.setPrompt("> ");
-  rl.prompt();
-
-  rl.on("line", async function (line) {
-    const response = await nlp.process(line);
-    console.log(response);
-    console.log(response.answer);
-    rl.prompt();
-  }).on("close", function () {
-    process.exit(0);
-  });
+  console.log("Chatbot started! you can chat with the bot using this link http://localhost:3000/");
 })();
